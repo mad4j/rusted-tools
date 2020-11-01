05417af702642717450b34c6f52128b1329a4444
Daniele Olmisani <mad4j@users.noreply.github.com>
2020-11-01 11:24:31 +0100

    update

--- today/src/main.rs
+++ today/src/main.rs
@@ -2,7 +2,7 @@ use chrono::{DateTime, Local};
 
 fn main() {
 
-    let now : DateTime<Local> = Local::now();
+    let now: DateTime<Local> = Local::now();
 
     print!("{}", now.format("%Y%m%d"));
 }
